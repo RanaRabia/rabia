3911ab527c399ae1fd19c152ce19cfac537d9573
rabia-rana <shayanrana696@gmail.com>
2024-10-24 12:03:49 +0500

    hello-everyone

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+
+const Home=()=>{
+    return(
+        <>
+        <h1>THIS IS NEW  WEBSITE PAGE</h1>
+        <p> I AM GOING TO MAKE NEW WEBSITE</p>
+        <h2>CONTENTS</h2>
+        <p>HOW ARE YOU</p>
+        <p>WHAT ARE U DOING</p>
+        <h3>BLESSINGS</h3>
+        <p>MAY U LIVE LONGER</p>
+        </>
+    )
+
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(){
+    return(
+        <div>
+            <h1>HELLLO EVERY ONE</h1>
+        </div>
+    )
+}
